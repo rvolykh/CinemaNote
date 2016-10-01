d50a9894dac75916373e674baff74b1209ae72a0
rvolykh <b.pomka@gmail.com>
2016-10-01 12:44:13 +0300

    Change a bit angular structure and convert EOLs

--- src/main/java/com/pikaso/home/cinemanote/util/MD5Util.java
+++ src/main/java/com/pikaso/home/cinemanote/util/MD5Util.java
@@ -10,15 +10,6 @@ public final class MD5Util {
 	private MD5Util(){}
 
 	public static String hash(String text){
-//		byte byteData[] = DigestUtils.md5Digest(text.getBytes());
-//
-//		//convert the byte to hex format method 1
-//		StringBuffer sb = new StringBuffer();
-//		for (int i = 0; i < byteData.length; i++) {
-//			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-//		}
-//
-//		return sb.toString();
 		return DigestUtils.md5DigestAsHex(text.getBytes()).toString();
 	}
 	
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,6 +1,13 @@
-angular.module('hello', []).controller('home', function($http) {
-	var self = this;
-	$http.get('resource/').then(function(response) {
-		self.greeting = response.data;
+
+angular.module('cinemaApp', [])
+.controller('MainController', ['$scope', '$http', function($scope, $http) {
+	$http.get('health/').then(function(response) {
+		$scope.health = response.data;
 	})
+}])
+.directive('mainComponent', function() {
+	return {
+		restrict: 'A',
+		template: 'Name: {{health.id}} Address: {{health.time}}'
+	};
 });
